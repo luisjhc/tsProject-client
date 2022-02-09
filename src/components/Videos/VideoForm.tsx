6ae47d17b63a893d8b7b17393df7e729d419a57f
luisjhc <ljoaquincuenca@gmail.com>
2022-02-09 13:08:30 +0100

    first commit

---
+++ src/components/Videos/VideoForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoForm = () => {
+  return <div>video Form</div>;
+};
+
+export default VideoForm;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VideoList from "./components/Videos/VideoList";
+import VideoForm from "./components/Videos/VideoForm";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VideoList />} />
+        <Route path="/new-video" element={<VideoForm />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
